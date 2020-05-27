b115ba99cdef544c82b8f7737e44f89366f016ad
Andi Koh <andikoh@amandakoh-mac-c02t24s1hf1r.corp.improbable.io>
2020-05-27 01:21:49 +0100

    Updates

---
+++ precache-manifest.65916eb289e1bfa701300bdda8a08578.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bfd7c2cc0c11767828cf7bf41dd91d",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "961a2b1507e6594b0d19",
+    "url": "/demo/static/css/main.939d3b3a.chunk.css"
+  },
+  {
+    "revision": "8332b13ac5a86b16323d",
+    "url": "/demo/static/js/2.c01a23a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.c01a23a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "961a2b1507e6594b0d19",
+    "url": "/demo/static/js/main.58c21ab2.chunk.js"
+  },
+  {
+    "revision": "7b949a04041847afc156",
+    "url": "/demo/static/js/runtime-main.edfb04a4.js"
+  },
+  {
+    "revision": "ee8c0d3d3162f0d22485fafa2f091e91",
+    "url": "/demo/static/media/bg-layers-bottom.ee8c0d3d.svg"
+  },
+  {
+    "revision": "b9dc863b9e02eb09b5b4b5bef9eb1364",
+    "url": "/demo/static/media/bg-layers-top.b9dc863b.svg"
+  },
+  {
+    "revision": "f4e2b9c68ba3853df64cdc2b7c5320cf",
+    "url": "/demo/static/media/data-api.f4e2b9c6.svg"
+  },
+  {
+    "revision": "01715fe6ac325adea5d8d0e2fd0e8750",
+    "url": "/demo/static/media/donate_direct.01715fe6.png"
+  },
+  {
+    "revision": "db4f49163acf8dcc1435da42a1caac88",
+    "url": "/demo/static/media/facebook.db4f4916.svg"
+  },
+  {
+    "revision": "1bf101c8afea8187195e1ad06aaa7143",
+    "url": "/demo/static/media/fca.1bf101c8.jpg"
+  },
+  {
+    "revision": "0d8d8b1706d5b9061c29080cab0ff810",
+    "url": "/demo/static/media/github.0d8d8b17.svg"
+  },
+  {
+    "revision": "ae50e3db2d173794039f15d47bede1a5",
+    "url": "/demo/static/media/linkedin.ae50e3db.svg"
+  },
+  {
+    "revision": "cf1d31dc0570d02d757502ece5eddc1f",
+    "url": "/demo/static/media/medium.cf1d31dc.svg"
+  },
+  {
+    "revision": "afc1c9fe6234542c5260df4e7e7a0e97",
+    "url": "/demo/static/media/payments-api.afc1c9fe.svg"
+  },
+  {
+    "revision": "5df82f0fb2136b94eba47db880b0cad0",
+    "url": "/demo/static/media/truelayer-logo.5df82f0f.svg"
+  },
+  {
+    "revision": "61e62ecae84babe01dea89e219510d9e",
+    "url": "/demo/static/media/twitter.61e62eca.svg"
+  },
+  {
+    "revision": "dec9bddc064247e6105fe11ab9eaa3a3",
+    "url": "/demo/static/media/visa.dec9bddc.jpg"
+  },
+  {
+    "revision": "001a7391aef62660f80f02688a51595b",
+    "url": "/demo/static/media/youtube.001a7391.svg"
+  }
+]);
